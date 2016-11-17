a0b6314d0ee895c6819cd8032a89dace217f9503
jonahss <jonahss@gmail.com>
2016-11-17 14:53:54 -0800

    refactor, component breakout, joint image

---
+++ dir/app/components/map.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
+import Leaflet from 'leaflet'
+
+console.log('icon', Leaflet.icon)
+
+export default class EazeMap extends React.Component {
+  render() {
+    var icon = Leaflet.icon({
+      iconUrl: 'joint.png',
+      iconSize: [12, 60]
+    })
+    var markers = this.props.orders.map((l) => {
+      return <Marker position={l} icon={icon}>
+        <Popup>
+          <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+        </Popup>
+      </Marker>
+    })
+
+    return (
+      <Map center={this.props.position} zoom={this.props.zoom}>
+        <TileLayer
+          url='http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}'
+          attribution='Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        />
+        {markers}
+      </Map>
+    )
+  }
+}
--- dir/app/initialize.js
+++ dir/app/initialize.js
@@ -1,7 +1,47 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from 'components/App';
+import EazeMap from 'components/map';
+
+const position = [37.7700, -122.3500]
+const zoom = 13.0
+
+var potential_orders = [ { latitude: 37.7615487, longitude: -122.4191631 },
+  { latitude: 37.7924472, longitude: -122.3995004 },
+  { latitude: 37.777607, longitude: -122.3909239 },
+  { latitude: 37.7613407, longitude: -122.4884015 },
+  { latitude: 37.7924472, longitude: -122.3995004 },
+  { latitude: 37.7924472, longitude: -122.3995004 },
+  { latitude: 37.7928599, longitude: -122.4158524 },
+  { latitude: 37.7924472, longitude: -122.3995004 },
+  { latitude: 37.7928599, longitude: -122.4158524 },
+  { latitude: 37.7615487, longitude: -122.4191631 },
+  { latitude: 37.7615487, longitude: -122.4191631 },
+  { latitude: 37.777607, longitude: -122.3909239 },
+  { latitude: 37.7616335, longitude: -122.3883623 },
+  { latitude: 37.7615487, longitude: -122.4191631 },
+  { latitude: 37.7852279, longitude: -122.404389 },
+  { latitude: 37.76143, longitude: -122.419605 },
+  { latitude: 37.7924472, longitude: -122.3995004 },
+  { latitude: 37.7256557, longitude: -122.4510586 },
+  { latitude: 37.748724, longitude: -122.419976 },
+  { latitude: 37.7924472, longitude: -122.3995004 } ]
+  .map((i) => {
+    return [i.latitude, i.longitude]
+  })
+
+var orders = []
+
+var renderView = function(orders) {
+  ReactDOM.render(React.createElement(EazeMap, {position, zoom, orders}), document.querySelector('#map-container'));
+}
+
+var addOrder = function() {
+  console.log('adding order')
+  orders.push(potential_orders[orders.length])
+  renderView(orders)
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.querySelector('#map-container'));
+  renderView(orders)
+  setInterval(addOrder, 1000)
 });
